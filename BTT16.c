82de20fbe4f70b86b17af4dc395f61ed78dec003
LeonRemy <leonremyy2308@gmail.com>
2026-06-24 11:49:39 +0700

    Khai báo các thành phố

---
+++ BTT16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+#define VO_CUC 999999
+#define SO_THANH_PHO 11
+
+char ten_thanh_pho[SO_THANH_PHO][20] = {
+    "Ha Noi", 
+    "Son Tay", 
+    "Thai Nguyen", 
+    "Bac Ninh", 
+    "Bac Giang",
+    "Uong Bi", 
+    "Hai Phong", 
+    "Hai Duong", 
+    "Hung Yen", 
+    "Phu Ly", 
+    "Hoa Binh"
+};
+
